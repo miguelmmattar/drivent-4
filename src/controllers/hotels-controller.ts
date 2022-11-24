04669cf2e0f18491fc9bce21a7a0fd56992f8de1
Miguel Faria <mattar1704miguel@gmail.com>
2022-11-24 10:03:37 -0300

    Paid ticket requirement included to GET /hotels

--- src/controllers/hotels-controller.ts
+++ src/controllers/hotels-controller.ts
@@ -4,11 +4,16 @@ import { Response } from "express";
 import httpStatus from "http-status";
 
 export async function getHotels(req: AuthenticatedRequest, res: Response) {
+  const { userId } = req;
+
   try {
-    const hotels = await hotelsService.getHotels();
+    const hotels = await hotelsService.getHotels(userId);
 
     return res.status(httpStatus.OK).send(hotels);
   } catch (error) {
+    if(error.statusText === "BadRequestError") return res.sendStatus(httpStatus.BAD_REQUEST);
+    if(error.statusText === "PaymentRequiredError") return res.sendStatus(httpStatus.PAYMENT_REQUIRED);
+    if(error.name === "NotFoundError") return res.sendStatus(httpStatus.NOT_FOUND);
     return res.sendStatus(httpStatus.NO_CONTENT);
   }
 }
--- src/services/hotels-service/index.ts
+++ src/services/hotels-service/index.ts
@@ -1,7 +1,28 @@
-import { notFoundError } from "@/errors";
+import { notFoundError, requestError } from "@/errors";
 import hotelsRepository from "@/repositories/hotels-repository";
+import enrollmentRepository from "@/repositories/enrollment-repository";
+import ticketRepository from "@/repositories/ticket-repository";
 
-async function getHotels() {
+async function getHotels(userId: number) {
+  const enrollment = await enrollmentRepository.findWithAddressByUserId(userId);
+    
+  if(!enrollment) {
+    throw notFoundError();
+  }
+
+  const ticket = await ticketRepository.findTicketByEnrollmentId(enrollment.id);
+  if (!ticket) {
+    throw notFoundError();
+  }
+
+  if(!ticket.TicketType.includesHotel) {
+    throw requestError(400, "BadRequestError");
+  }
+
+  if(ticket.status !== "PAID") {
+    throw requestError(402, "PaymentRequiredError");
+  }
+  
   const hotels = await hotelsRepository.findHotels();
 
   if (!hotels) {
